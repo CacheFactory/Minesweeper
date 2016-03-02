85208efdbd283faa07feb7d66fc09566fd2d1511
Edward Anderson <ejanderson4@gmail.com>
2016-03-02 14:48:01 -0800

    initial commit

---
+++ game-solver.js
@@ -0,0 +1,36 @@
+
+Array.min = function( array ){
+  return Math.min.apply( Math, array );
+};
+var GameSolver = function(game){
+  this.game = game;
+}
+
+GameSolver.prototype.solve = function(){
+  while(this.game.status() == 'on'){
+    var squareSums = {};
+    var openSquares = this.game.openSquares();
+    for(var i in openSquares){
+      var square = openSquares[i];
+      var sum = square.neighbors.reduce(function(memo, square){
+        if(square.exposed){
+          return memo + square.number;
+        }else{
+          return memo;
+        }
+        
+      },0)
+      sum = sum;
+      squareSums[sum] = square;
+    }
+
+    var keys = _.keys(squareSums);
+    var leastKey = Array.min(keys);
+    var squareToPick = squareSums[leastKey];
+    
+    squareToPick.expose();
+  }
+
+  var win = this.game.status() == 'win';
+  return win;
+}
---
+++ spec/minesweeper-spec.js
@@ -0,0 +1,68 @@
+describe('Minesweeper', function(){
+  it("should create a board", function() {
+    var game = new Game(10,10,5)
+    expect(game.columns).toBe(10);
+    expect(game.rows).toBe(10);
+    expect(game.neededSquaresToWin).toBe(95);
+  });
+
+  it("should win when all squares have been exposed", function() {
+    var game = new Game(10,10,1)
+    
+    for(var r in game.grid){
+      for(var c in game.grid[r]){
+        var square = game.grid[r][c]
+        if(!square.hasMine){
+          square.expose()
+        }
+      }
+    }
+    expect(game.lose).toBe(false)
+    expect(game.win).toBe(true)
+  });
+
+  it("should lose when a squares have been exposed that has a bomb", function() {
+    var game = new Game(10,10,1)
+    
+    for(var r in game.grid){
+      for(var c in game.grid[r]){
+        var square = game.grid[r][c]
+        if(square.hasMine){
+          square.expose()
+        }
+      }
+    }
+
+    expect(game.win).toBe(false)
+    expect(game.lose).toBe(true)
+  });
+
+  it("should calculate neighboring squares correctly", function() {
+    var game = new Game(10,10,10)
+    
+    for(var r in game.grid){
+      for(var c in game.grid[r]){
+        var square = game.grid[r][c]
+        
+        var expectedNumber = square.neighbors.reduce(function(memo, neighbor){
+          var number = (neighbor.hasMine) ? 1 : 0;
+          return memo + number;
+        },0)
+        if(square.hasMine){
+          expect(square.number).toBe(0)
+        }else{
+          expect(square.number).toBe(expectedNumber)
+        }
+        
+      }
+    }
+  });
+
+  it("should expose neighboring squares correctly", function(){
+    spyOn(Game.prototype, 'createMineIndixies').and.returnValue([1,2,3,4,5])
+    var game = new Game(10,10,5)
+    game.grid[8][8].expose()
+    expect(game.openSquares().length).toBe(5)
+  })
+
+})
